349f01fedb6c45df210d8c06d137efab2631beb0
kimdohyun <fleekwhat@naver.com>
2025-06-16 16:07:13 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,34 @@
+package test.main;
+
+
+// MainClass 라는 이름의 클래스 만들기 
+public class MainClass {
+	
+	// main 이라는 이름의 메소드를 클래스 안에 만들기 
+	public static void main(String[] args) {
+		// main() 메소드 안에 있는 코드를 실행하려면 run 버튼 누르면 된다.
+		System.out.println("Hello, World!");
+		System.out.println("김도현");
+		
+		// let num = 10
+		int num = 10;
+		num = 20;
+		num = 30;
+		// num = 10.1
+		double num2 = 10.1;
+		num2 = 20.1;
+		
+		boolean isRun = true;
+		isRun = false;
+		
+		String myName = "김도현";
+		myName = "해골";
+		
+		System.out.println(myName);
+		System.out.println(isRun);
+		System.out.println(num2);
+		System.out.println(num);
+		
+		
+	}
+}
